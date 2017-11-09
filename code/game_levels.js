626b353b223ec1ed48ed6d4139266c003e3823cd
JulioRomani <33134382+JulioRomani@users.noreply.github.com>
2017-11-09 11:25:59 -0500

    Add files via upload

---
+++ code/game_levels.js
@@ -0,0 +1,29 @@
+var GAME_LEVELS = [
+  ["                                                                                ",
+   "                                              xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx  ",
+   "                                            xxxxxx         xxxxxxxxxxxxxxxxxxx  ",
+   "                                           xxxxx                            xx  ",
+   "                                          xxx                               xx  ",
+   "  xxxxxxxxxxxxxxxxxxxxxxx!!!!!!!!!!!!!!!xxxx                              o xx  ",
+   "  xxxxxxxxxxxxxxxxxxxxxxxx!!!!!!!!!!!!xxxx                     xxxxxxxxxxxxxxx  ",
+   "  xxxx                   xxx!!!!!!!!!xxx                          xxx     xxxx  ",
+   "  xx                       xxx!!!!!xxx           xxx!!!!!xxxo      x       xxx  ",
+   "  xx                         xx!!!xx          o    xx!!!xx                  xx  ",
+   "  xx                          xx!xx          xxx    xxvxx                   xx  ",
+   "  xxx                          xvx                                          xx  ",
+   "  xxx            o                                                          xx  ",
+   "  xx           xxxx                        o                     yyyy       xx  ",
+   "  xx        s xx                          xxx                               xx  ",
+   "  xx        xxxxx                       xx!!!xx                           o xx  ",
+   "  xx   @   xxxxxxx       s o          xxx!!!!!xxx          o         xxxxxxxxx  ",
+   "  xxxxxxxxxxxxxxx!!!!!!xxxxxxx     xxxxx!!!!!!!xxxxx     xxxxx     xxx      xx  ",
+   "  xxxxxxxxxxxxxxxvxxxvxxxxxxxx!!!!!xxx!!!!!xxx!!xxxx!!!!!!!!!!!!!!!xx     xxxx  ",
+   "  xxx       xx          xxxxxxx!!!xxxxvxxvxxxxxvxxxxxx!!!!!!!!!!!!xxx       xx  ",
+   "  xx                     xx   x!!!x     x  xxx     xxxxvxxx!xvxxvxxxxxx     xx  ",
+   "  xx                           xvx          x             xvx               xx  ",
+   "                                                                           xxx  ",
+   "                                                                                "]
+  ];
+
+if (typeof module != "undefined" && module.exports)
+  module.exports = GAME_LEVELS;
